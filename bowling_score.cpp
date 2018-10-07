164dde9e623d9233b2d8b1b36bd7704e1df3f604
sswst12 <shantiwendler@gmail.com>
2018-10-07 16:21:07 -0400

    Simplify scoring loop and error checking.

--- bowling_score.cpp
+++ bowling_score.cpp
@@ -3,122 +3,92 @@
 
 std::vector<int> parse_input(int argc, char *argv[])
 {
-  /* Check to see if the number of throws are within the limits of a
-   * valid game before we bother to parse it.
-   */
-  if(argc < 13)
-    throw std::invalid_argument("too few throws entered");
- 
-  if(argc > 22)
-    throw std::invalid_argument("too many throws entered");
+	/* Check to see if the number of rolls are within the limits of a
+	 * valid game before we bother to parse it.
+	 */
+	if (argc < 13)
+		throw std::invalid_argument("too few rolls entered");
 
-  /* Parse the input into integers with error checking. */
-  int n_throws = argc - 1;
-  std::vector<int> pins_per_throw(n_throws);
-  for(int i=0;i< n_throws;i++) {
-    char *end;
-    auto n_pins = strtol(argv[i + 1], &end, 10);
-    if(*end != '\0' || n_pins > 10 || n_pins < 0) {
-      throw std::invalid_argument("# of pins knocked down must be"
-        " valid base 10 integer between 0 and 10");
-    }
-	pins_per_throw[i] = n_pins;
-  }
-  return pins_per_throw;
+	if (argc > 22)
+		throw std::invalid_argument("too many rolls entered");
+
+	/* Parse the input into integers with error checking. */
+	int n_rolls = argc - 1;
+	std::vector<int> rolls(n_rolls);
+	for (int i = 0; i < n_rolls; i++) {
+		char *end;
+		auto n_pins = strtol(argv[i + 1], &end, 10);
+		if (*end != '\0' || n_pins > 10 || n_pins < 0) {
+			throw std::invalid_argument("# of pins knocked down must be"
+				" valid base 10 integer between 0 and 10");
+		}
+		rolls[i] = n_pins;
+	}
+	return rolls;
+}
+
+/* Add up rolls from start to end indices to score the frame. */
+int score_frame(const std::vector<int>& rolls, int start, int end)
+{
+	int score = 0;
+	/* Make sure we don't overrun the rolls vector. */
+	if (end >= rolls.size()) throw std::invalid_argument("too few rolls entered");
+	if (end <= start) throw std::invalid_argument("frame should include at least 2 rolls");
+	for (int i = start; i <= end; i++) score += rolls[i];
+	return score;
+}
+
+int score_game(const std::vector<int>& rolls)
+{
+	int n_rolls = rolls.size();
+	int total = 0;
+
+	/* Starting and ending indices for rolls in the current frame. */
+	int start = 0, end = 1;
+
+	std::cout << "Scorecard: ";
+	/* Expect 10 frames, process one frame at a time, looking at a sliding window from start to end. */
+	for (int iframe = 0; iframe < 10; iframe++) {
+		/* At start of new frame, we should have at least two rolls left to process. */
+		if (start + 1 >= n_rolls) throw std::invalid_argument("too few rolls entered");
+		bool is_strike = rolls[start] == 10;
+		bool is_spare = rolls[start] + rolls[start + 1] == 10;
+		if (is_strike || is_spare) {
+			end = start + 2;
+			total += score_frame(rolls, start, end);
+		}
+		else {
+			end = start + 1;
+			int score = score_frame(rolls, start, end);
+			if (score > 10) throw std::invalid_argument("Two rolls in the same frame"
+				" cannot add up to more than 10 pins");
+			total += score;
+		}
+		/* Determine start of next frame. */
+		if (is_strike) start += 1;
+		else start += 2;
+		std::cout << "[" << total << "]";
+	}
+	/* Should have processed all rolls. */
+	if (end != n_rolls - 1) throw std::invalid_argument("too many rolls entered");
+	std::cout << " --> " << total << std::endl;
+	return total;
 }
 
 int main(int argc, char** argv)
-try{
-  auto pins_per_throw = parse_input(argc, argv);
-  int frames_scored = 0;
-  int i = 0;
-  int total = 0;
-  int last_frame = 0;
-  bool last_was_strike = false;
-  int n_throws = pins_per_throw.size();
-  std::cout << "Scorecard: ";
-
-  /* Look at a sliding window of up to 3 throws at a time to 
-   * score a frame.
-   *
-   * Note: if there was no strike or spare on the last frame,
-   * this will exit before the last frame has been tallied.
-   */
-  while(i < (n_throws - 2)) {
-	last_was_strike = false;
-    /* This should never reach 10 since i > n_throws-2
-	 * if we've scored all the way through the 10th frame
-	 * for a valid number of throws. */
-    if(frames_scored > 9)
-      throw std::invalid_argument("too many throws entered");
-    /* Look at the first throw and see if it's a strike. */
-    int first = pins_per_throw[i];
-    /* Add the next two throws and score the frame. */
-    if(first == 10) {
-      last_frame = first + pins_per_throw[i + 1] + pins_per_throw[i + 2];
-      /* There was only one throw in this frame, set i to the
-       * first throw in the next frame. */
-      i += 1;
-	  last_was_strike = true;
-    }
-    else {
-      /* We don't have a strike, so at least two throws count
-       * in this frame (which can't add up to more than 10), and
-       * the start of the next frame is i+2.*/
-		last_frame = first + pins_per_throw[i + 1];
-      if(last_frame > 10)
-        throw std::invalid_argument("Two throws in the same frame"
-          " cannot add up to more than 10 pins");
-      if(last_frame == 10) last_frame += pins_per_throw[i + 2];
-      i += 2;
-    }
-    total += last_frame;
-    std::cout << "[" << total << "] ";
-    frames_scored++;
-  }
-  /* Handle last frame if it was a non-spare/strike
-   * and finish checking for invalid number of throws. */
-  if(frames_scored < 9) {
-    /* We should have either processed all frames or all but
-     *  the last frame. */
-    throw std::invalid_argument("too few throws entered");
-  }
-  if(frames_scored == 9) {
-    /* No strike or spare on the last frame, so we should have
-     *  exactly two throws left to process. */
-    if(i != n_throws - 2)
-      throw std::invalid_argument("too few throws entered");
-    /* Go ahead and add up the last frame. */
-	int first_ball = pins_per_throw[i];
-	int second_ball = pins_per_throw[i + 1];
-    last_frame = first_ball + second_ball;
-    total += last_frame;
-	if (first_ball == 10 || last_frame == 10)
-		throw std::invalid_argument("too few throws entered");
-	else if(last_frame > 10)
-		throw std::invalid_argument("Two throws in the same frame"
-			" cannot add up to more than 10 pins");
-	std::cout << "[" << total << "] ";
-  }
-  else {
-	  /* Check to see if there were extra throws after
-	   * scoring the final strike/spare. */
-	  if (last_was_strike && i != n_throws - 2)
-		throw std::invalid_argument("too many throws entered");
-	  if(!last_was_strike && i != n_throws - 1)
-		throw std::invalid_argument("too many throws entered");
-  }
-  std::cout << " --> " << total;
-  return 0;
+try {
+	auto rolls = parse_input(argc, argv);
+	int total = score_game(rolls);
+	return 0;
 }
-catch(std::exception& e) {
-  std::cout << "Error: " << e.what() << std::endl;
-  return 1;
+catch (std::exception& e) {
+	std::cout << "Error: " << e.what() << std::endl;
+	return 1;
 }
 
-catch(...) {
-  std::cout << "Error: unexpected exception" << std::endl;
-  return 1;
+catch (...) {
+	std::cout << "Error: unexpected exception" << std::endl;
+	return 1;
 }
 
 
